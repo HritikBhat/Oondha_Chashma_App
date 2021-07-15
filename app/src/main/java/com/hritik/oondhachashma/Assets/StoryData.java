b8ccf88c08c59af6b6ee968f74e5c3a250174455
HritikBhat <bhathritik399@gmail.com>
2021-07-15 13:39:28 +0530

    Added new episodes

--- app/src/main/java/com/hritik/oondhachashma/Assets/StoryData.java
+++ app/src/main/java/com/hritik/oondhachashma/Assets/StoryData.java
@@ -150,6 +150,130 @@ public class StoryData {
         epList.add("7-LMqUtS3wg");
         storiesDataList.add(new Story(10,"Play Outside Tapu Sena!! Episode","https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep369.jpg",epList));
 
+
+        epList = new ArrayList<>();
+        epList.add("DTWXjxBZsBk");
+        epList.add("RueyjLfmj0Y");
+        epList.add("IuLMmairawk");
+        epList.add("cyh23pyThMo");
+        epList.add("FF0Z--IFHAE");
+        storiesDataList.add(new Story(11,"Sakharam Building Color Renovation Episode","https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep759.jpg",epList));
+
+
+        epList = new ArrayList<>();
+        epList.add("KQHvWwqyx9M");
+        epList.add("FMNn54F_WPM");
+        epList.add("fB41uUNPrWw");
+        epList.add("KwbxHc_zpvU");
+        epList.add("cY3OxAH8bpY");
+        epList.add("retY8ZH04pA");
+        epList.add("VktLY8uAm2E");
+        epList.add("JN_Y7fwTk38");
+        epList.add("ymywyumT9uw");
+        epList.add("GXr6iPZ_Dk8");
+        epList.add("4yuPn4aa1do");
+        epList.add("kQSxor1UAd8");
+        epList.add("gKOHw_FAguM");
+        epList.add("hQuLmJZyzFk");
+        epList.add("OwRlOWAgb8c");
+        epList.add("gGU9qDWQTYQ");
+        epList.add("JbS3IJIRKAQ");
+        epList.add("W05fJh7wTUU");
+        epList.add("cKVUQ7tsQuA");
+        epList.add("zbySvxknSqY");
+
+        storiesDataList.add(new Story(12,"Sheru The Dog Episode","https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep1103.jpg",epList));
+
+        epList = new ArrayList<>();
+        epList.add("hbC_yHHGIM0");
+        epList.add("igRdRujw-Ik");
+        epList.add("MpnVDf9EIBY");
+        epList.add("81e8wJCfxU0");
+        epList.add("zv4OOZqmfu4");
+
+
+        storiesDataList.add(new Story(13,"Sunder's 1 Lakh Scheme Episode","https://i.ytimg.com/vi/hbC_yHHGIM0/maxresdefault.jpg",epList));
+
+        epList = new ArrayList<>();
+        epList.add("YN3zdloZbeQ");
+        epList.add("zVe8HzT5Md0");
+        epList.add("SntESUovHI4");
+        epList.add("fAyyiym_KxE");
+        epList.add("Oi48hHqWFm4");
+
+        storiesDataList.add(new Story(14,"Lohri Makarsankrati Special Episode","https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep87.jpg",epList));
+
+
+        epList = new ArrayList<>();
+        epList.add("71LAHN--wvU");
+        epList.add("xdi9QA4N5Es");
+        epList.add("g34x76AacSE");
+        epList.add("Ec5OH0plyLE");
+        epList.add("Q2UNEd6yUIs");
+        epList.add("JhEqDlLLiMQ");
+        epList.add("V81FgVUZ5m8");
+        epList.add("V9NsOFbGc30");
+
+
+        storiesDataList.add(new Story(15,"Rukmini & Lovely Episode","https://i.ytimg.com/vi/FbfQZemOo88/hqdefault.jpg",epList));
+
+        epList = new ArrayList<>();
+        epList.add("Pc2732Hh0mc");
+        epList.add("D6EWlfZJtF0");
+        epList.add("p4C9sVMEyqc");
+        epList.add("-Apugwu-5bY");
+        epList.add("cISrISogVqk");
+        epList.add("kfjggOIlo2k");
+        epList.add("Gqh5Z6mEbEc");
+        epList.add("0LoCpC1r_JU");
+
+        storiesDataList.add(new Story(16,"Miss Call Babita Episode","https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep545.jpg",epList));
+
+        epList = new ArrayList<>();
+        epList.add("c0DE9Qf3rSE");
+        epList.add("sVH3tBqM3tk");
+        epList.add("21eQtStLSRI");
+        epList.add("U2WGD4iSXqY");
+        epList.add("dldPewBC4ps");
+        epList.add("6TVS-K7GT3w");
+        epList.add("KuxNLEeX3gs");
+        epList.add("VFyRVR8_y3A");
+        storiesDataList.add(new Story(17,"Chandiramani Flat Auction Episode","https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep477.jpg",epList));
+
+        epList = new ArrayList<>();
+        epList.add("a90n8gtRwh4");
+        epList.add("rAxCLQlcJH0");
+        epList.add("KVeraYzEM3A");
+        epList.add("Ib_JAHrow3w");
+        epList.add("YnoiR2QdMvI");
+        epList.add("y7H8Su2LCHk");
+        epList.add("4O9RklUyUpQ");
+        epList.add("qCeLiip6ZAs");
+        epList.add("fZqUFQ3xYsA");
+        epList.add("0b_NFMygWBY");
+        epList.add("1_9bpVUnUcg");
+        epList.add("kGLtN66uMEI");
+        epList.add("wlpNh5t_1rM");
+        epList.add("SI6vfbdMNEs");
+        epList.add("u7uCG16DGIk");
+        epList.add("iXyff5HsT3Y");
+        epList.add("QQeS6355EWI");
+        epList.add("umn45w0SooQ");
+        epList.add("1nbWi6pJoVQ");
+        epList.add("NHbzmFtYZ0g");
+        epList.add("Ix6D5wuB604");
+        epList.add("3SmS4vS5fSE");
+        epList.add("5dxAChYm8Ks");
+        epList.add("fMx8FQd1SEQ");
+        epList.add("cnmIIPNAEsg");
+        epList.add("lGQ_FP3dF3s");
+        epList.add("TD4Zy2oRMUs");
+
+
+
+        storiesDataList.add(new Story(18,"Gulabo Episode",
+                "https://origin-staticv2.sonyliv.com/landscape_thumb/clip_sab_tmkoc_jetha_daya_gulabo_moments_ep1030_landscape_Thumb.jpg",epList));
+
     }
 
     public ArrayList<Story> getStoriesDataList() {
